b1995d3e64eb04f3d209b886aa03a3f04c6673e0
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-06-05 16:48:12 -0400

    first commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Dev environment for 3D seismic wave propogation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    custom-nixpkgs.url = "github:Open-Systems-Innovation/custom-nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, custom-nixpkgs, ... }:
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ custom-nixpkgs.overlays.default ];
+        };
+      in
+        {
+          devShells.${system}.default = pkgs.mkShell {
+            name = "default";
+               
+            packages = [
+            # General packages
+              # pkgs.hello-nix
+               pkgs.petsc
+               pkgs.mpich
+               pkgs.clang-tools
+              #  # Python packages
+              #(pkgs.python3.withPackages (python-pkgs: [
+              #  # packages for formatting/ IDE
+              #  python-pkgs.pip
+              #  python-pkgs.python-lsp-server
+              #  # packages for code
+              #  python-pkgs.gmsh
+              #  python-pkgs.matplotlib
+              #  python-pkgs.meshio
+              #  python-pkgs.numpy
+              #  python-pkgs.firedrake
+              #]))
+            ];
+
+            PETSC_DIR = "${pkgs.petsc}";
+
+            shellHook = ''
+              export ENVIRONMENT_NAME="3D Seismic Wave Propogation"
+              export PS1="┌─[\[\e[01;32m\]\u\[\e[00m\]@\[\e[01;32m\]\h\[\e[00m\]:\[\e[1;34m\]\w\[\e[0m\]][\$ENVIRONMENT_NAME]\n└─╼"
+            '';
+          };
+        };
+}
